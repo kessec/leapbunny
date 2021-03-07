f2f04f25775cf76665cb5394325dcb309ca29271
Mukhtar Kiboigo <mukhtarkiboigo@gmail.com>
2021-03-07 01:24:24 -0800

    [tweak] removal of target/rootfs leapbunny_packages_base-fs at libSDL · kessec_leapbunny#	deleted:    target/rootfs/LF/Base/DidjMDLs/MAC_CLIP6/icon.png

--- target/rootfs/var/0.6.0.fs
+++
@@ -1,5 +0,0 @@
-0x00000000 0x00100000 payload/emerald-boot.bin
-0x00100000 0x00100000 payload/HEALTH_AND_SAFETY.32.rle
-0x00200000 0x00800000 payload/kernel.cbf
-0x00a00000 0x05600000 payload/erootfs.ubi
-0x06000000 0x1a000000 payload/bulk.ubi 
