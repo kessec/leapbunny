cbfc3f141d9f7e813f3c8ce92cddc1765b1d03fb
Mukhtar Kiboigo <mukhtarkiboigo@gmail.com>
2021-03-28 15:53:01 -0700

    [feature] Added SDL Path to /etc/profile.

--- packages/base-fs/install.sh
+++ packages/base-fs/install.sh
@@ -16,10 +16,10 @@ fi
 # parse args
 set_standard_opts $*
 pushd $PROJECT_PATH/packages/base-fs
-tar -xf base-fs-0.2-A.tgz -C ./base-fs-0.2-A
+tar -xf base-fs-0.2.1.tar.gz -C ./base-fs-0.2.1
 
 # Copy the contents of the base filesystem to target.
-cp -r ./base-fs-0.2-A/* $PROJECT_PATH/target/rootfs
+cp -r ./base-fs-0.2.1/* $PROJECT_PATH/target/rootfs
 echo "Successfully copied base-fs"
 chmod +rwx $PROJECT_PATH/target/rootfs/*
 
