8403fc7991a2a8b84c3082a191707bc2fb57d0a3
Mukhtar Kiboigo <mukhtarkiboigo@gmail.com>
2021-03-28 18:15:21 -0700

    [bugfix] Fixed Tar Unpacking Bug.

--- packages/base-fs/install.sh
+++ packages/base-fs/install.sh
@@ -16,7 +16,7 @@ fi
 # parse args
 set_standard_opts $*
 pushd $PROJECT_PATH/packages/base-fs
-tar -xf base-fs-0.2.1.tar.gz -C ./base-fs-0.2.1
+tar -xf base-fs-0.2.1.tar.gz -C ./
 
 # Copy the contents of the base filesystem to target.
 cp -r ./base-fs-0.2.1/* $PROJECT_PATH/target/rootfs
